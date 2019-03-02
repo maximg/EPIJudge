97687e44366a333abba12ca9b0121ab9fd59ee9e
Maksim Golov <maxim.golov@gmail.com>
2019-03-02 12:53:13 +0100

    6.14 is valid sudoku

--- epi_judge_cpp/is_valid_sudoku.cc
+++ epi_judge_cpp/is_valid_sudoku.cc
@@ -1,9 +1,42 @@
 #include <vector>
 #include "test_framework/generic_test.h"
 using std::vector;
+
 // Check if a partially filled matrix has any conflicts.
 bool IsValidSudoku(const vector<vector<int>>& partial_assignment) {
-  // TODO - you fill in here.
+  auto& A = partial_assignment;
+
+  for (int i = 0; i < 9; ++i) {
+    int seen = 0;
+    for (int j = 0; j < 9; ++j) {
+      if (A[i][j]) {
+        if (seen & (1 << A[i][j])) return false;
+        seen |= 1 << A[i][j];
+      }
+    }
+  }
+  for (int j = 0; j < 9; ++j) {
+    int seen = 0;
+    for (int i = 0; i < 9; ++i) {
+      if (A[i][j]) {
+        if (seen & (1 << A[i][j])) return false;
+        seen |= 1 << A[i][j];
+      }
+    }
+  }
+
+  for (int k = 0; k < 9; k += 3)
+    for (int m = 0; m < 9; m += 3) {
+      int seen = 0;
+      for (int i = k; i < k+3; ++i) {
+        for (int j = m; j < m+3; ++j) {
+          if (A[i][j]) {
+            if (seen & (1 << A[i][j])) return false;
+            seen |= 1 << A[i][j];
+          }
+        }
+      }
+    }
   return true;
 }
 
