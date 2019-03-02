921d56901200dc35b724ba0c7285376ed9ed0d9d
Maksim Golov <maxim.golov@gmail.com>
2019-03-02 13:30:20 +0100

    6.22 phone number mnemonic

--- epi_judge_cpp/phone_number_mnemonic.cc
+++ epi_judge_cpp/phone_number_mnemonic.cc
@@ -1,11 +1,37 @@
 #include <string>
 #include <vector>
+#include <queue>
 #include "test_framework/generic_test.h"
 using std::string;
 using std::vector;
 vector<string> PhoneMnemonic(const string& phone_number) {
-  // TODO - you fill in here.
-  return {};
+    vector<string> M =
+        { "0", "1", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ" };
+
+    if (phone_number.empty())
+        return {};
+
+    vector<string> perms;
+    int pos = 0;
+    std::queue<string> bfs;
+    bfs.push({});
+    while (!bfs.empty()) {
+        string next = bfs.front();
+        bfs.pop();
+        if (next.length() == phone_number.length()) {
+            perms.push_back(next);
+            continue;
+        }
+
+        int pos = next.length();
+        char c = phone_number[pos];
+        if (c == '*' || c == '#')
+            bfs.push(next + c);
+        else for (char alpha: M[c - '0'])
+            bfs.push(next + alpha);
+    }
+
+    return perms;
 }
 
 int main(int argc, char* argv[]) {
