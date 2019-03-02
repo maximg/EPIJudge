ca9fd70666e7816f4392e8d8432708d737ea2b52
Maksim Golov <maxim.golov@gmail.com>
2019-03-02 21:53:58 +0100

    9.5: tree inorder O(1)

--- epi_judge_cpp/tree_with_parent_inorder.cc
+++ epi_judge_cpp/tree_with_parent_inorder.cc
@@ -4,8 +4,51 @@
 using std::vector;
 
 vector<int> InorderTraversal(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return {};
+    using NodePtr = BinaryTreeNode<int>*;
+    if (!tree) return {};
+
+    // we keep a pointer to the previous node to check where we are coming to a node
+    vector<int> out;
+
+    NodePtr prev = nullptr;
+    NodePtr cur = tree.get();
+
+    while (true) {
+        enum { LEFT, RIGHT, UP } dir;
+
+        if (prev == cur->parent) {
+            if (cur->left)
+                dir = LEFT;
+            else 
+                dir = RIGHT;
+        }
+        else
+        if (prev == cur->left.get())
+            dir = RIGHT;
+        else {
+            assert( prev == cur->right.get());
+            dir = UP;
+        }
+        prev = cur;
+
+        switch (dir) {
+            case LEFT: cur = cur->left.get(); break;
+            case RIGHT:
+                out.push_back(cur->data);
+                if (cur->right) {
+                    cur = cur->right.get();
+                    break;
+                }
+                // Fallthrough
+            case UP: {
+                if (cur->parent)
+                    cur = cur->parent;
+                else
+                    return out;
+            }
+        }
+    }
+    return out;
 }
 
 int main(int argc, char* argv[]) {
