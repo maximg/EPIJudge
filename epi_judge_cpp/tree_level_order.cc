c14d23397cc4a3ad802ca920d5efd8e9c264d059
Maksim Golov <maxim.golov@gmail.com>
2019-03-02 19:08:19 +0100

    8.9: tree level order (bfs)

--- epi_judge_cpp/tree_level_order.cc
+++ epi_judge_cpp/tree_level_order.cc
@@ -7,8 +7,32 @@ using std::vector;
 
 vector<vector<int>> BinaryTreeDepthOrder(
     const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return {};
+    if (!tree)
+        return {};
+
+    vector<vector<int>> out;
+
+    std::queue<BinaryTreeNode<int>*> bfs;
+    bfs.push(nullptr);
+    bfs.push(tree.get());
+
+    while (!bfs.empty()) {
+        auto cur = bfs.front();
+        bfs.pop();
+        if (cur) {
+            out.back().push_back(cur->data);
+            if (cur->left)
+                bfs.push(cur->left.get());
+            if (cur->right)
+                bfs.push(cur->right.get());  
+        }
+        else if(!bfs.empty()) {
+            out.push_back({});
+            bfs.push(nullptr);
+        }
+    }
+
+    return out;
 }
 
 int main(int argc, char* argv[]) {
