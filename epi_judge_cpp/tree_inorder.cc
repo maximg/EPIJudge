513179f2acc6032faada54c37aeddae7784a0d25
Maksim Golov <maxim.golov@gmail.com>
2019-03-02 18:19:09 +0100

    8.3: tree inorder with a stack

--- epi_judge_cpp/tree_inorder.cc
+++ epi_judge_cpp/tree_inorder.cc
@@ -6,8 +6,32 @@ using std::unique_ptr;
 using std::vector;
 
 vector<int> InorderTraversal(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return {};
+  if (!tree) return {};
+
+  using NodePtr = const BinaryTreeNode<int>*;
+  vector<int> result;
+  vector<std::pair<NodePtr, int>> stack;
+
+  stack.push_back({tree.get(), 0});
+  while (!stack.empty()) {
+    auto& current = stack.back();
+    if (current.second == 0) {
+        current.second = 1;
+        // if we can, go left
+        if (current.first->left)
+            stack.push_back({current.first->left.get(), 0});
+    }
+    else if (current.second == 1) {
+        result.push_back(current.first->data);
+        current.second = 2;
+        if (current.first->right)
+            stack.push_back({current.first->right.get(), 0});
+    }
+    else // 2
+        stack.pop_back();
+  }
+
+  return result;
 }
 
 int main(int argc, char* argv[]) {
