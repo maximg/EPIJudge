25c4da5ac38224505c7ddfc368c17632fe56c84d
Maksim Golov <maxim.golov@gmail.com>
2019-03-03 12:02:50 +0100

    10.1: merge sorted arrays

--- epi_judge_cpp/sorted_arrays_merge.cc
+++ epi_judge_cpp/sorted_arrays_merge.cc
@@ -1,9 +1,42 @@
 #include <vector>
+#include <queue>
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::pair;
+
+template<>
+struct std::greater<pair<int, int>>
+{
+    bool operator() (const pair<int, int>& a, const pair<int, int>& b) const {
+        return a.second > b.second;
+    }
+};
+
 vector<int> MergeSortedArrays(const vector<vector<int>>& sorted_arrays) {
-  // TODO - you fill in here.
-  return {};
+
+    if (sorted_arrays.empty()) return {};
+    
+    std::priority_queue<pair<int, int>,
+                        vector<pair<int, int>>,
+                        std::greater<pair<int, int>>> minHeap;
+    vector<vector<int>::const_iterator> heads;
+
+    for (int i = 0; i < sorted_arrays.size(); ++i) {
+        heads.push_back(sorted_arrays[i].begin());
+        if (heads[i] != sorted_arrays[i].end())
+            minHeap.push({i, *(heads[i]++)});
+    }
+
+    vector<int> out;
+    while (!minHeap.empty()) {
+        out.emplace_back(minHeap.top().second);
+        int i = minHeap.top().first;
+        minHeap.pop();
+        if (heads[i] != sorted_arrays[i].end())
+            minHeap.push({i, *(heads[i]++)});            
+    }
+
+    return out;
 }
 
 int main(int argc, char* argv[]) {
