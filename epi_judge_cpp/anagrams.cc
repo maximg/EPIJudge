08981ae45ade92feb724f4bef00fae752a413fc3
Maksim Golov <maxim.golov@gmail.com>
2019-03-04 18:34:02 +0100

    12.7: anagrams

--- epi_judge_cpp/anagrams.cc
+++ epi_judge_cpp/anagrams.cc
@@ -1,12 +1,31 @@
 #include <string>
 #include <vector>
+#include <unordered_map>
 #include "test_framework/generic_test.h"
 using std::string;
 using std::vector;
+using namespace std;
 
 vector<vector<string>> FindAnagrams(const vector<string>& dictionary) {
-  // TODO - you fill in here.
-  return {};
+    vector<vector<string>> anagrams;
+    unordered_map<string, size_t> index;
+
+    for (const auto& word : dictionary) {
+        string key = word;
+        sort(key.begin(), key.end());
+        auto it = index.find(key);
+        if (it != index.end()) {
+            anagrams[it->second].emplace_back(word);
+        }
+        else {
+            anagrams.emplace_back(vector<string>{word});
+            index[key] = anagrams.size() - 1;
+        }
+    }
+    anagrams.erase(partition(anagrams.begin(), anagrams.end(),
+                            [](const auto& vec) { return vec.size() > 1; }),
+                   anagrams.end());
+    return anagrams;
 }
 
 int main(int argc, char* argv[]) {
