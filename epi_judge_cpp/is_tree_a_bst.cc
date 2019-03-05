d68d338a4a8e1f871093e48724031b0ddf59cd9e
Maksim Golov <maxim.golov@gmail.com>
2019-03-05 19:41:44 +0100

    14.1: is tree a BST

--- epi_judge_cpp/is_tree_a_bst.cc
+++ epi_judge_cpp/is_tree_a_bst.cc
@@ -3,9 +3,26 @@
 #include "test_framework/generic_test.h"
 using std::unique_ptr;
 
+// The tree must be a BST within the range lower / upper
+bool helper(const unique_ptr<BinaryTreeNode<int>>& node, int lower, int upper) {
+    if (!node)
+        return true;
+
+    if (node->data < lower || node->data > upper)
+        return false;
+
+    return helper(node->left, lower, node->data) && 
+           helper(node->right, node->data, upper);
+}
+
+
 bool IsBinaryTreeBST(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return true;
+    // BST property is: all nodes to the left are less than root
+    // all nodes to the right are greater than root
+
+    // We could check this recursively, adding an upper bound and lower bound
+
+    return helper(tree, INT_MIN, INT_MAX);
 }
 
 int main(int argc, char* argv[]) {
