5104af536ada382dc07d6bab98620e6cc78df98f
Maksim Golov <maxim.golov@gmail.com>
2019-03-06 22:51:30 +0100

    15.11: Levenstein distance

--- epi_judge_cpp/levenshtein_distance.cc
+++ epi_judge_cpp/levenshtein_distance.cc
@@ -4,9 +4,26 @@
 using std::string;
 using std::vector;
 
+
 int LevenshteinDistance(const string& A, const string& B) {
-  // TODO - you fill in here.
-  return 0;
+    if (A.empty() || B.empty())
+        return std::max(A.size(), B.size());
+
+    vector<int> cost(A.size()+1);
+    iota(cost.begin(), cost.end(), 0);
+ 
+    for (int i = 0; i < B.size(); ++i) {
+        vector<int> newcost(A.size()+1);
+        newcost[0] = cost[0] + 1;
+
+        for (int j = 0; j < A.size(); ++j) {
+            newcost[j+1] = std::min( std::min(cost[j+1]+1, newcost[j]+1),
+                cost[j] + (A[j] == B[i] ? 0 : 1));
+        }
+        std::swap(cost, newcost);
+    }
+
+    return cost.back();
 }
 
 int main(int argc, char* argv[]) {
