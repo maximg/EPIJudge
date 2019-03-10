cc71e0c27ba35a96b9982b1eb964738adc246556
Maksim Golov <maxim.golov@gmail.com>
2019-03-10 10:59:12 +0100

    17.8: solve sudoku

--- epi_judge_cpp/sudoku_solve.cc
+++ epi_judge_cpp/sudoku_solve.cc
@@ -8,10 +8,96 @@
 using std::begin;
 using std::end;
 using std::vector;
+
+
+class Sudoku {
+  using Board = vector<vector<int>>;
+  Board& board;
+  vector<vector<int>> missingDigs;
+  vector<vector<int>> spaces;
+
+  int blockStart[9] = {0,0,0,3,3,3,6,6,6};
+  int blockEnd[9] = {3,3,3,6,6,6,9,9,9};
+
+public:
+  explicit Sudoku(vector<vector<int>>& board) : board(board) {
+    for (int i = 0; i < board.size(); ++i) {
+      vector<bool> digs(10);
+      spaces.push_back({});
+      for (int j = 0; j < board.size(); ++j) {
+        if (board[i][j] > 0)
+          digs[board[i][j]] = true;
+        else
+          spaces.back().push_back(j);
+      }
+      missingDigs.push_back({});
+      for (int j = 1; j <= 9; ++j)
+        if (!digs[j])
+          missingDigs.back().push_back(j);
+    }
+  }
+
+  bool solve() {
+    return processRow(0);
+  }
+
+private:
+  bool checkCol(int col, int dig) {
+    for (int i = 0; i < board.size(); ++i)
+      if (board[i][col] == dig)
+        return false;
+    return true;
+  }
+
+  bool checkBlock(int row, int col, int dig) {
+
+    for (int i = blockStart[row]; i < blockEnd[row]; ++i )
+      for (int j = blockStart[col]; j < blockEnd[col]; ++j )
+        if (board[i][j] == dig)
+          return false;
+    return true;
+  }
+
+  bool placeDigs(int row, const vector<int>& missing) {
+    for (int i = 0; i < missing.size(); ++i) {
+      int col = spaces[row][i];
+      if (!checkCol(col, missing[i]) || !checkBlock(row, col, missing[i]) )
+        return false;
+      else
+        board[row][col] = missing[i];
+    }
+    return true;
+  };
+
+  bool processRow(int row) {
+    if (row == board.size())
+        return true;
+
+    // build vectors of missing digits per row
+    // for each row, go through permutations of possible digits
+    // for each permutation try adding digits to the puzzle
+    // abort as soon as there is a violation
+    // if all digits are placed, move to the next row
+    // if next row fails, try the next permutation
+
+    // create copies
+    auto origRow = board[row];
+    auto missing = missingDigs[row];
+    do {
+      if (placeDigs(row, missing) && processRow(row + 1))
+        return true;
+      board[row] = origRow;
+    }
+    while (next_permutation(missing.begin(), missing.end()));
+
+    return false;
+  }
+};
+
 bool SolveSudoku(vector<vector<int>>* partial_assignment) {
-  // TODO - you fill in here.
-  return true;
+  return Sudoku(*partial_assignment).solve();
 }
+
 vector<int> GatherColumn(const vector<vector<int>>& data, size_t i) {
   vector<int> result;
   for (auto& row : data) {
