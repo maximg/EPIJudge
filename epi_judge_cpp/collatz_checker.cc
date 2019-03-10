87cada4b5a0f2e13124074f1be915360ffd25a2b
Maksim Golov <maxim.golov@gmail.com>
2019-03-10 12:06:16 +0100

    17.12: Collatz checker

--- epi_judge_cpp/collatz_checker.cc
+++ epi_judge_cpp/collatz_checker.cc
@@ -2,9 +2,28 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
+bool collatzHelper(vector<bool>& collatz, long long n) {
+  if (collatz.size() <= (n+1) / 2)
+    collatz.resize((n+1)/2, false);
+
+  if (collatz[(n-1)/2])
+    return true;
+
+  if (n & 1) {
+    collatz[(n-1)/2] = collatzHelper(collatz, 3 * n + 1);
+    return collatz[(n-1)/2];
+  }
+  else
+    return collatzHelper(collatz, n / 2);
+}
+
 bool TestCollatzConjecture(int n) {
-  // TODO - you fill in here.
-  return false;
+    vector<bool> collatz(n, false);
+    collatz[0] = true;
+    for (int i = 2; i <= n; ++i)
+        if (!collatzHelper(collatz, i))
+            return false;
+    return true;
 }
 
 int main(int argc, char* argv[]) {
