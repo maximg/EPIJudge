f638f21493c8fb38cd6fc654fee021bb345c094e
Maksim Golov <maxim.golov@gmail.com>
2019-03-10 18:51:46 +0100

    6.9: BigInt multiply

--- epi_judge_cpp/int_as_array_multiply.cc
+++ epi_judge_cpp/int_as_array_multiply.cc
@@ -1,11 +1,51 @@
 #include <vector>
 #include "test_framework/generic_test.h"
+#include <cmath>
 using std::vector;
 vector<int> Multiply(vector<int> num1, vector<int> num2) {
-  // TODO - you fill in here.
-  return {};
-}
+    vector<vector<int>> prods(num2.size());
 
+    int sign = num1[0] * num2[0] > 0 ? 1 : -1;
+    num1[0] = abs(num1[0]);
+    num2[0] = abs(num2[0]);
+
+    for (int j = 0; j < num2.size(); ++j) {
+        for (int i = 0; i < num1.size(); ++i) {
+            prods[j].push_back(num1[i] * num2[j]);
+        }
+    }
+
+    vector<int> res;
+    for (int d = num1.size() + num2.size() - 2; d >= 0; --d) {
+        res.push_back(0);
+        for (int j = 0; j < num2.size(); ++j) {
+            if (d - j < num1.size() && d-j >= 0)
+                res.back() += prods[j][d-j];
+        }
+    }
+
+    long long carry = 0;
+    for (int i = 0; i < res.size(); ++i) {
+        res[i] += carry;
+        if (res[i] > 9) {
+            int d = res[i] % 10;
+            carry = std::floor(res[i] / 10.0);
+            res[i] = d;
+        }
+        else
+            carry = 0;
+    }
+    while (carry) {
+        res.push_back(carry % 10);
+        carry = std::floor(carry / 10.0);
+    }
+    while (res.size() > 1 && res.back() == 0)
+        res.resize(res.size() - 1);
+
+    res.back() *= sign;
+
+    return vector<int>(res.rbegin(), res.rend());
+}
 int main(int argc, char* argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"num1", "num2"};
