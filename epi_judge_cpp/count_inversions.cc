104fd0a8ebf46ec2da03bfa044aabedb2c092bf9
Maksim Golov <maxim.golov@gmail.com>
2019-03-23 16:15:48 +0100

    15.2: count inversions

--- epi_judge_cpp/count_inversions.cc
+++ epi_judge_cpp/count_inversions.cc
@@ -2,9 +2,24 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
+int helper(vector<int>::const_iterator begin, vector<int>::const_iterator end) {
+    size_t dist = std::distance(begin, end);
+    if (dist < 2) return 0;
+    if (dist == 2) return (*begin > *(end-1)) ? 1 : 0;
+    auto mid = begin + dist / 2;
+    int count = helper(begin, mid) + helper(mid, end);
+    vector<int> helpV(mid, end);
+    std::sort(helpV.begin(), helpV.end());
+    for (auto it = begin; it != mid; ++it) {
+        int k = std::distance(helpV.begin(), 
+                              std::lower_bound(helpV.begin(), helpV.end(), *it));
+        count += k;
+    }
+    return count;
+}
+
 int CountInversions(vector<int> A) {
-  // TODO - you fill in here.
-  return 0;
+    return helper(A.begin(), A.end());
 }
 
 int main(int argc, char* argv[]) {
