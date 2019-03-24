6805cfd0ba34a632e47bc92acb71e3d2a6254175
Maksim Golov <maxim.golov@gmail.com>
2019-03-24 12:01:28 +0100

    17.4: defective jugs

--- epi_judge_cpp/defective_jugs.cc
+++ epi_judge_cpp/defective_jugs.cc
@@ -1,4 +1,5 @@
 #include <vector>
+#include <unordered_map>
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
 using std::vector;
@@ -7,8 +8,34 @@ struct Jug {
   int low, high;
 };
 bool CheckFeasible(const vector<Jug>& jugs, int L, int H) {
-  // TODO - you fill in here.
-  return true;
+    std::unordered_map<int, int> M;
+    for (auto& j: jugs) {
+        if (M.find(j.low) == M.end())
+            M[j.low] = j.high;
+        else
+            M[j.low] = std::min(M[j.low], j.high);
+    }
+
+    // dp[i] indicates the max possible error if the min error is i
+    // we allow for min error to be greater than L so we need H elements
+    vector<int> dp(H);
+    for (size_t i = 0; i < H; ++i) {
+        if (M.find(i) != M.end())
+            dp[i] = M[i];
+
+        for (size_t j = 0; j < i ; ++j) {
+            if (dp[j] != 0 && M.find(i-j) != M.end()) {
+                if (dp[i] > 0)
+                    dp[i] = std::min(dp[i], dp[j] + M[i-j]);
+                else
+                    dp[i] = dp[j] + M[i-j];            }
+        }
+
+        if (i >=L && dp[i] > 0 && dp[i] <= H)
+            return true;
+    }
+
+  return false;
 }
 template <>
 struct SerializationTraits<Jug> : UserSerTraits<Jug, int, int> {};
