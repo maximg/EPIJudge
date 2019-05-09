0aa74c11d0d0e1c488d4bf863365ab43345bbdc6
Maksim Golov <maxim.golov@gmail.com>
2019-05-09 22:44:49 +0200

    15.14: binomial coefficients

--- epi_judge_cpp/binomial_coefficients.cc
+++ epi_judge_cpp/binomial_coefficients.cc
@@ -1,8 +1,26 @@
 #include "test_framework/generic_test.h"
 
+#include <cassert>
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        auto t = b;
+        b = a % b;
+        a = t;
+    }
+    return a;
+}
+
 int ComputeBinomialCoefficient(int n, int k) {
-  // TODO - you fill in here.
-  return 0;
+
+    int binom = 1;
+    for (int i = k + 1; i <= n; ++i) {
+        int div = i - k;
+        int gcd_i = gcd(i, div);
+        div /= gcd_i;
+        binom = (binom / div) * (i / gcd_i);
+    }
+    return binom;
 }
 
 int main(int argc, char* argv[]) {
